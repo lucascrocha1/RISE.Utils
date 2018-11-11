7d51605c0653e2b6420c5464c75407ac8e8d2745
Lucas da Silva Costa Rocha <lucascrocha@outlook.com>
2018-11-11 18:24:49 -0200

    Add project files.

---
+++ RISE.Utils/QueryableExtensions/QueryableExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace RISE.Utils.QueryableExtensions
+{
+    using System.Linq;
+
+    public static class QueryableExtensions
+    {
+        /// <summary>
+        /// Paginates the query
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query">Query</param>
+        /// <param name="pageSize">Size of the page</param>
+        /// <param name="pageIndex">Index of the page</param>
+        /// <returns></returns>
+        public static IQueryable<T> Paginate<T>(this IQueryable<T> query, int pageSize, int pageIndex)
+        {
+            return query
+                .Skip((pageIndex - 1) * pageSize)
+                .Take(pageSize);
+        }
+    }
+}
