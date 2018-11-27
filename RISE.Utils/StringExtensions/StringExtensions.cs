5d490735bb4f87ed66afc019aa87502b28c13d86
Lucas da Silva Costa Rocha <lucascrocha@outlook.com>
2018-11-27 08:21:36 -0200

    Add string extension

---
+++ RISE.Utils/StringExtensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace RISE.Utils.StringExtensions
+{
+    public static class StringExtensions
+    {
+        public static string FormatToken(this string source)
+        {
+            if (string.IsNullOrEmpty(source))
+                return source;
+
+            var array = source.ToCharArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == '.')
+                    array[i] = '|';
+            }
+
+            return new string(array);
+        }
+    }
+}
