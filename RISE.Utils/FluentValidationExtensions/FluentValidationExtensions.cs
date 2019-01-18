92acbecbf8cb5eeb7610384d4eb738c928e45e1a
Lucas Rocha <lucascrocha@outlook.com>
2019-01-18 21:14:31 -0200

    Adds fluent validation extensions

---
+++ RISE.Utils/FluentValidationExtensions/FluentValidationExtensions.cs
@@ -0,0 +1,40 @@
+﻿namespace RISE.Utils.FluentValidationExtensions
+{
+    using FluentValidation;
+    using MediatR;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Linq;
+    using System.Reflection;
+
+    public class FluentValidationExtensions<T> where T : class
+    {
+        public static void RegisterFluentValidation(IServiceCollection services)
+        {
+            services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+            RegisterValidators(services);
+        }
+
+        private static void RegisterValidators(IServiceCollection services)
+        {
+            var assembly = typeof(T).GetTypeInfo().Assembly;
+            var validatorType = typeof(IValidator);
+
+            var validators = assembly
+                .GetExportedTypes()
+                .Where(e => validatorType.IsAssignableFrom(e) && !e.IsInterface);
+
+            foreach (var validator in validators)
+            {
+                services.AddTransient(validator);
+
+                var validatorInterfaces = validator
+                    .GetInterfaces()
+                    .Where(e => e.IsGenericType && e.GetGenericTypeDefinition() == typeof(IValidator<>));
+
+                foreach (var validatorInterface in validatorInterfaces)
+                    services.AddTransient(validatorInterface, validator);
+            }
+        }
+    }
+}
